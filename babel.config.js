cdd17456ada6adfa77f4ab8f1053ede7fb82219c
morgenweck <morgenweck@gmail.com>
2019-12-28 11:48:04 -0500

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: [
+    '@vue/app',
+  ]
+}
+
+// module.exports = {
+//   presets: [
+//     '@vue/app', ['@babel/env', {
+//       useBuiltIns: 'entry'
+//     }]
+//   ]
+// }
+//module.exports = {
+// presets: ['@babel/preset-env', '@vue/app']
+//}
+
+// module.exports = {
+//   [
+//     ["@vue/app", {
+//       useBuiltIns: "entry"
+//     }]
+//   ]
+// }
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//     devServer: {
+//         proxy: {
+//             "/route": {
+//                 target: "http://localhost:8081",
+//             }
+//         }
+//     }
+// }
+
+module.exports = {
+    transpileDependencies: ['ansi-regex']
+}
